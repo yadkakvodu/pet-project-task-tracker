a1a97b3fd76e04ce770cf0df37ca380aea6d5b30
dinislamalipkachev <dalipkachev@inbox.ru>
2026-04-13 22:32:04 +0300

    2 commit

--- src/User.java
+++ src/User.java
@@ -2,8 +2,20 @@ import java.sql.*;
 
 public class User {
 
+    private static final String URL = "db.url";
+    private static final String PASSWORD = "db.password";
+    private static final String USERNAME = "db.username";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try (Connection connection = DriverManager.getConnection(
+                PropertiesUtil.get(URL),
+                PropertiesUtil.get(USERNAME),
+                PropertiesUtil.get(PASSWORD)
+        )) {
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
 
