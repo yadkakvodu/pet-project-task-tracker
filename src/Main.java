b6500bc550ac0953494b51f78de7d2971fa4c1e3
dinislamalipkachev <dalipkachev@inbox.ru>
2026-04-18 17:55:08 +0300

    7 commit (createProject fix, add method addUserAtProject)

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,21 @@ public class Main {
     public static void main(String[] args) {
 //        UserService.userRegistration("RASKOL", "TJV1312", "pravoslavna@mail.ru");
 
-        User user1 = new User("Yaroslav", "Samusevich", "YarikPercosetik2007");
-        int user1Id = UserService.authorization(user1);
-        System.out.println(user1Id);
+//        User user1 = new User("Yaroslav", "Samusevich", "YarikPercosetik2007");
+//        User user2 = new User("Dinislam Alipkachev", "dinislamchik@mail.ru", "Dinislam200720072007");
+//        UserService.userRegistration(user2);
 
-//        System.out.println(UserService.executeQuery("users"));
+        User user3 = new User("Konstantin Gulin", "KGulin@inbox.ru", "Prajskaya1312");
+//        UserService.userRegistration(user3);
+//        ProjectService.createProject(user3, "MongoDBDriver", "Driver for Mongo DataBase");
+
+        User user4 = new User("Semyon Voronin", "SVO2022@mail.ru", "ZaRussiaZaPutina2022");
+
+        UserService.userRegistration(user4);
+
+        ProjectService.addUserAtProject(user3, user4, 1);
+
+
 
     }
 
--- src/ProjectService.java
+++ src/ProjectService.java
@@ -1,6 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import javax.xml.transform.Result;
+import java.sql.*;
 
 public class ProjectService {
 
@@ -11,11 +10,109 @@ public class ProjectService {
     private static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(
                 PropertiesUtil.get(URL),
-                PropertiesUtil.get(PASSWORD),
-                PropertiesUtil.get(USERNAME));
+                PropertiesUtil.get(USERNAME),
+                PropertiesUtil.get(PASSWORD));
     }
 
+    protected static void createProject(User user, String name, String description) {
 
+        String SQL = "INSERT INTO projects(name, description, owner_id) VALUES (?, ?, ?);";
+        String SQL2 = "INSERT INTO project_members(project_id, role, user_id) VALUES(?, ?, ?);";
+
+        if (UserService.authorization(user) > 0) {
+
+            try (Connection connection = getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(SQL, PreparedStatement.RETURN_GENERATED_KEYS);
+                 PreparedStatement preparedStatement2 = connection.prepareStatement(SQL2)) {
+
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, description);
+                preparedStatement.setInt(3, UserService.authorization(user));
+
+                preparedStatement.executeUpdate();
+
+                var id = preparedStatement.getGeneratedKeys();
+                int id_2 = 0;
+                while (id.next()) {
+                    id_2 = id.getInt(1);
+                }
+
+                preparedStatement2.setInt(1, id_2);
+                preparedStatement2.setString(2, "admin");
+                preparedStatement2.setInt(3, UserService.authorization(user));
+
+                if (id_2 > 0) {
+                    preparedStatement2.executeUpdate();
+                } else {
+                    System.out.println("ошибка");
+                }
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+
+//    ## ✅ Добавление пользователя в проект
+//
+//     один пользователь добавляет другого
+//
+//    Код должен:
+//
+//            1. проверить: добавляющий — admin
+//2. проверить: пользователь существует
+//3. проверить: его ещё нет в проекте
+//4. добавить в project_members
+
+
+    protected static void addUserAtProject(User admin, User member, Integer project_id) {
+
+        String SQL = "SELECT role FROM project_members WHERE project_id = ? AND user_id = ?";
+        String SQL2 = "SELECT user_id FROM project_members WHERE project_id = ?";
+        String SQL3 = "INSERT INTO project_members(project_id, role, user_id) VALUES(?, ?, ?)";
+
+        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+             PreparedStatement preparedStatement2 = connection.prepareStatement(SQL2);
+             PreparedStatement preparedStatement3 = connection.prepareStatement(SQL3)) {
+
+            preparedStatement.setInt(1, project_id);
+            preparedStatement.setInt(2, UserService.authorization(admin));
+
+            preparedStatement2.setInt(1, project_id);
+            ResultSet resultSet2 = preparedStatement2.executeQuery();
+
+            int id = 0;
+            while (resultSet2.next()) {
+                if (resultSet2.getInt("user_id") == UserService.authorization(member)) {
+                    id++;
+                }
+            }
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                if (resultSet.getString("role").equals("admin")) {
+                    if (UserService.authorization(member) > 0 && id == 0) {
+                        preparedStatement3.setInt(1, project_id);
+                        preparedStatement3.setString(2, "member");
+                        preparedStatement3.setInt(3, UserService.authorization(member));
+
+                        preparedStatement3.executeUpdate();
+                        System.out.println("Пользователь добавлен");
+                    } else {
+                        System.out.println("Пользователь есть в проекту!");
+                    }
+                } else {
+                    System.out.println("Только админ может добавлять участника в проект!");
+                }
+            } else {
+                System.out.println("Неизвестная ошибка");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
 
 
